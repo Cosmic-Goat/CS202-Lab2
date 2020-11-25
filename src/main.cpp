f2340f7852ec7025ebf3842bc52e8350251093a9
Shrey <shrey.pasricha@gmail.com>
2020-11-25 05:32:51 -0500

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,41 +5,39 @@
 #include "RoundRobin.hpp"
 #include "SRJF.hpp"
 
-static void TimingSnapshot(const size_t cycle, std::vector<Process> &processes, std::ostream &output)
-{
-	constexpr std::string_view STATE_STRINGS[] = {"running", "ready", "blocked"};
-	output << cycle << " ";
-	for (auto &&p : processes)
-	{
-		if (p.state != Process::Inactive) output << p.id << ":" << STATE_STRINGS[p.state] << " ";
-	}
-	output << "\n";
-}
-
+// Scheduling algorithm runner, keeps track of stats and printing cycles, actual driver code is in the (sub)classes
 template<typename T>
 static void run(std::vector<Process> &processes, std::ostream &output)
 {
+	constexpr std::string_view STATE_STRINGS[] = {"running", "ready", "blocked"};
+	
 	T scheduler(processes);
+	
 	scheduler.runCycle();
 	while (scheduler.isRunning())
 	{
-		TimingSnapshot(scheduler.getCurCycle(), processes, output);
+		// Print Cycle Snapshot
+		output << scheduler.getCurCycle() << " ";
+		for (auto &&p : processes)
+		{
+			if (p.state != Process::Inactive) output << p.id << ":" << STATE_STRINGS[p.state] << " ";
+		}
+		output << "\n";
+		
+		// Run next cycle
 		scheduler.runCycle();
 	}
 	
+	// Print Stats
 	output << "\nFinishing time: " << scheduler.getCurCycle() - 1;
 	output << "\nCPU utilization: " << static_cast<float>(scheduler.activeCycles) / scheduler.getCurCycle();
-	
-	for (auto &&p : scheduler.finishedList)
-	{
-		output << "\nTurnaround process " << p.first << ": " << p.second;
-	}
-	
+	for (auto &&p : scheduler.finishedList) output << "\nTurnaround process " << p.first << ": " << p.second;
 	output << std::endl;
 }
 
+// Variadic template function to pick which scheduling algorithm (Scheduler subclass) to use from the given int
 template<typename... Ts>
-constexpr void runScheduler(int algorithm, Ts &... args)
+constexpr void runScheduler(size_t algorithm, Ts &... args)
 {
 	constexpr void (*algoList[])(std::vector<Process> &, std::ostream &) = {run<FCFS>, run<RoundRobin>, run<SRJF>};
 	algoList[algorithm](args...);
@@ -47,6 +45,7 @@ constexpr void runScheduler(int algorithm, Ts &... args)
 
 int main(int argc, char *argv[])
 {
+	// I/O & file handling
 	std::string file(argv[1]);
 	std::ifstream input(file);
 	std::ofstream output;
@@ -58,6 +57,7 @@ int main(int argc, char *argv[])
 		size_t num;
 		input >> num;
 		
+		// Read all proccess information from file
 		std::vector<Process> processes(num);
 		for (auto &p : processes) input >> p.id >> p.cpuTime >> p.ioTime >> p.arrival;
 		

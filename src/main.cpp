3f8861d00fd4ade3e6cbff9f2f0ee2ab7657dead
Shrey <shrey.pasricha@gmail.com>
2020-11-24 22:25:17 -0500

    yay templates

--- src/main.cpp
+++ src/main.cpp
@@ -34,24 +34,26 @@ void TimingSnapshot(const size_t cycle, std::ostream &output, std::vector<Proces
 	}
 	output << "\n";
 }
-//
-//void run(Scheduler &scheduler, std::ostream &output, std::vector<Process> &processes)
-//{
-//	scheduler.runCycle();
-//	while (scheduler.isRunning())
-//	{
-//		TimingSnapshot(scheduler.getCurCycle(), output, processes);
-//		scheduler.runCycle();
-//	}
-//
-//	output << "\nFinishing time: " << scheduler.getCurCycle() - 1;
-//	output << "\nCPU utilization: " << static_cast<float>(scheduler.activeCycles) / scheduler.getCurCycle();
-//
-//	for (auto &&p : scheduler.finishedList)
-//	{
-//		output << "\nTurnaround process " << p.first << ": " << p.second;
-//	}
-//}
+
+template<typename T>
+void run(std::ostream &output, std::vector<Process> &processes)
+{
+	T scheduler(processes);
+	scheduler.runCycle();
+	while (scheduler.isRunning())
+	{
+		TimingSnapshot(scheduler.getCurCycle(), output, processes);
+		scheduler.runCycle();
+	}
+
+	output << "\nFinishing time: " << scheduler.getCurCycle() - 1;
+	output << "\nCPU utilization: " << static_cast<float>(scheduler.activeCycles) / scheduler.getCurCycle();
+
+	for (auto &&p : scheduler.finishedList)
+	{
+		output << "\nTurnaround process " << p.first << ": " << p.second;
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -72,29 +74,12 @@ int main(int argc, char *argv[])
 			input >> id >> cpuTime >> ioTime >> arrival;
 			processes.emplace_back(id, cpuTime, ioTime, arrival);
 		}
-	
-		std::unique_ptr<Scheduler> scheduler;
 		
 		switch (std::stoi(argv[2]))
 		{
 			case 0:
-				scheduler = std::make_unique<FCFS>(processes);
+				run<FCFS>(output, processes);
 				break;
-		}
-		
-		scheduler->runCycle();
-		while (scheduler->isRunning())
-		{
-			TimingSnapshot(scheduler->getCurCycle(), output, processes);
-			scheduler->runCycle();
-		}
-		
-		output << "\nFinishing time: " << scheduler->getCurCycle() - 1;
-		output << "\nCPU utilization: " << static_cast<float>(scheduler->activeCycles) / scheduler->getCurCycle();
-		
-		for (auto &&p : scheduler->finishedList)
-		{
-			output << "\nTurnaround process " << p.first << ": " << p.second;
 		}
 	}
 	return 0;
